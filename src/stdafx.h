3a0655447d4b9f9cce3c55c2fb630b863f2c4929
Marc Hernandez <marcsh@gmail.com>
2020-10-29 20:57:27 -0700

    Initial import

---
+++ src/stdafx.h
@@ -0,0 +1,35 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#pragma warning(disable : 4100)
+
+#include <stdio.h>
+#include <tchar.h>
+
+// TODO: reference additional headers your program requires here
+
+// Needs to be in front of everything
+#include "../../cblib/stl_basics.h"
+
+
+#include <Windows.h>
+
+// TODO: reference additional headers your program requires here
+#include <memory>
+#include <chrono>
+#include <cstdint>
+#include <memory>
+#include <vector>
+#include <cassert>
+#include <optional>
+
+
+#include "cblib_includes.h"
+
+#include "pham_core.h"
